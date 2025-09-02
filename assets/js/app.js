4fb32aa87b23f2f0408c839e5a5344a0032bb6f8
rohanpatil3890-a11y <rohanpatil3890@gmail.com>
2025-09-02 17:04:49 +0530

    update responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,17 +17,17 @@ const setRating = (rating) =>{
 
 
 moviesArry.forEach(movie =>{
-	result += `<div class="col-md-4 mb-4">
+	result += `<div class="col-lg-4 col-md-6 col-sm-12 mb-4">
 	    <div class="card">
 		 <figure class="MovieCard mb-0">
 		  <img src= ${movie.poster_path}>
 		  <figcaption>
 		   <div class="titleInfo">
 		    <div class="row">
-			 <div class="col-md-10">
+			 <div class="col-10">
 			  <h2>${movie.original_title}</h2>
 			 </div>
-			  <div class="col-md-2">
+			  <div class="col-2">
 			   <span class="badge ${setRating(movie.vote_average)}">${movie.vote_average}</span>
 			  </div>
 			</div>
@@ -40,7 +40,7 @@ moviesArry.forEach(movie =>{
 		 </figure>
 		</div>
 	   </div>
-	  </div> `;
+	  </div> `
 });
 
 
