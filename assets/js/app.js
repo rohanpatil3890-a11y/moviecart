4cbbd5821ff6b0715757868009725798e98b8821
rohanpatil3890-a11y <rohanpatil3890@gmail.com>
2025-09-02 16:20:49 +0530

    moviecard

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+const cl = console.log;
+
+
+
+let result = "";
+
+const setRating = (rating) =>{
+	
+	if(rating >= 7){
+		return "badge-success";
+	}else if(rating >= 5 && rating < 7){
+		return "badge-warning";
+	}else{
+		return "badge-danger";
+	}
+};
+
+
+moviesArry.forEach(movie =>{
+	result += `<div class="col-md-4 mb-4">
+	    <div class="card">
+		 <figure class="MovieCard mb-0">
+		  <img src= ${movie.poster_path}>
+		  <figcaption>
+		   <div class="titleInfo">
+		    <div class="row">
+			 <div class="col-md-10">
+			  <h2>${movie.original_title}</h2>
+			 </div>
+			  <div class="col-md-2">
+			   <span class="badge ${setRating(movie.vote_average)}">${movie.vote_average}</span>
+			  </div>
+			</div>
+			</div>
+			<div class="overview">
+			 <h5>${movie.title}</h5>
+			 <p>${movie.overview}</p>
+			</div>
+		  </figcaption>
+		 </figure>
+		</div>
+	   </div>
+	  </div> `;
+});
+
+
+const postContainer = document.getElementById("postContainer");
+
+postContainer.innerHTML = result;
